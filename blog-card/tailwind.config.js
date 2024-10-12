74dfda101118f56029cc0189edb735a1945c631d
Helal <imhelal.dev@gmail.com>
2024-10-12 17:31:17 +0600

    first commit

---
+++ blog-card/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        figtree: ['"Figtree"', "serif"],
+        // Add more custom font families as needed
+      },
+    },
+  },
+  plugins: [],
+};
